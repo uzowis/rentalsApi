271cae8bfb827fb818900494c641822029773a0f
Wisdom Aturuobi <uzowis@gmail.com>
2021-04-02 08:47:04 +0100

    Adding authorization feature

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,19 @@
 const jwt = require('jsonwebtoken');
+const config = require('config');
 
-const token = req.header('x-auth-token');
 
-if(!token) return res.status(401).send('No token Provided');
+function auth(req, res, next) {
+    const token = req.header('x-auth-token');
+    if(!token) return res.status(401).send('Access Denied, No token Provided');
+
+    try {
+        const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+        req.user = decoded;
+        next();
+    } catch (ex) {
+        res.status(400).send('Invalid Access Token');
+    }
+}
+
+
+module.exports = auth;
